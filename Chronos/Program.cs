1bfb665d4e60f66e9cefdfc1178f2087fc9b6d90
mrverdinsky <goncalo.vila.verde@hotmail.com>
2022-04-07 12:31:54 +0100

    Add Stopwatch exercise

---
+++ Chronos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch cron1 = new Stopwatch();
+            Stopwatch cron2 = new Stopwatch();
+            cron1.Start();
+            Thread.Sleep(1000);
+            cron2.Start();
+            Thread.Sleep(1000);
+            cron1.Stop();
+            cron2.Stop();
+            Console.WriteLine($"Stopwatch 1: {cron1.ElapsedMilliseconds}");
+            Console.WriteLine($"Stopwatch 2: {cron2.ElapsedMilliseconds}");
+
+        }
+    }
+}
