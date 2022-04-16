606896fbcd26f01e2af0390f8b298846dac8b69f
MrVerdinsky <goncalo.vila.verde@hotmail.com>
2022-04-16 14:45:52 +0100

    Add MyTestOfRandoms

---
+++ MyTestOfRandoms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyTestOfRandoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initiates a random instance
+            Random random = new Random();
+
+            //Asks the user for a number of dices
+            Console.Write("Insert number of dices to roll: ");
+            byte nDices = Byte.Parse(Console.ReadLine());
+
+            //Initiates the variable used to sum all dices
+            int totalRolled = 0;
+
+            //Loop used to sum each rolled dice based on
+            //number of dices input by the user
+            for (int i = 0; i < nDices; i++)
+            {
+               
+                int diceRoll = random.Next(1, 7);
+                Console.WriteLine($"Dice {i + 1}: {diceRoll}");
+                totalRolled += diceRoll;
+            }
+
+            //Prints result of sum on screen
+            Console.WriteLine($"Total rolled: {totalRolled}");
+        }
+    }
+}
